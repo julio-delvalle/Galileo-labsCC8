34d43e588b7c93a6c5edd3b1b6076422135e080c
julio-delvalle <delvallejulio199@gmail.com>
2024-07-18 15:52:21 -0600

    Implements basic response, simple GET requests with media and html sent correctly

--- Lab02_HTTP/Lab02/Response.java
+++ Lab02_HTTP/Lab02/Response.java
@@ -1,35 +1,110 @@
 import java.io.*;
+import java.net.FileNameMap;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 import java.util.*;
-import java.nio.file.*;
-import java.util.stream.*;
 import java.util.logging.*;
+import java.util.stream.*;
 
 public class Response {
-    // ======================================
-    // No modifica la firma de la función.
-    public void sendData (Logger LOGGER, PrintStream dataOut, Integer nThreadServer, Object request) throws Exception {
-    // ======================================
+    public void sendData(Logger LOGGER, PrintStream dataOut, Integer nThreadServer, Object request) throws Exception {
+        int errorCode = 200;
 
-        /////////////////////////////////////////////////
-        //                                             // 
-        //   Procesar el cuerpo de la Respuesta aquí   //
-        //                                             //
-        /////////////////////////////////////////////////
-        String fileData = new String(Files.readAllBytes(Paths.get("./www/index.html")));
-        fileData = fileData.replace("{fieldTest_DEMO}", "El servidor cambió esto y agregó un número aleatorio: "+ (new Random()).nextInt(1000)  );
-        ArrayList<String> response = new ArrayList<String>();
-        response.add("HTTP/1.1 200 OK");
-        response.add("Content-Type: text/html");
-        response.add("ClaseCC8: Alumnos");
-        response.add("Content-Length: " + fileData.length());
-        response.add("");
-        response.add(fileData);
-        dataOut.print( response.stream().collect(Collectors.joining("\r\n")) );
-        LOGGER.info("(" + nThreadServer + ") response: " + response ); 
-        LOGGER.info("(" + nThreadServer + ") request: " + request );
-        // ESTE ES UN EJEMPLO ESTATICO 
-        // con lo minimo en el Header
-        // Y RESPONDE LO MISMO A TODO
+        LOGGER.info(request.toString());
+        String[] requestArr = request.toString().split("\\|\\|", 0); 
 
-    }// sendData
+        LOGGER.info(requestArr[0]);
+        LOGGER.info(requestArr[1]);
+        LOGGER.info(requestArr[requestArr.length - 1]);
+
+        String firstLine = requestArr[0];
+        String body = requestArr[requestArr.length - 1];
+
+        String method = firstLine.split(" ")[0];
+        String path = firstLine.split(" ")[1];
+        String httpVersion = firstLine.split(" ")[2];
+
+        LOGGER.info(method);
+        LOGGER.info(path);
+        LOGGER.info(httpVersion);
+
+        path = path.split("\\?")[0];
+
+        if (path.endsWith("/")) {
+            path += "index.html";
+        }
+
+
+
+        String fileData = " ";
+        byte[] fileDataBytes = new byte[0];
+        try {
+            fileData = new String(Files.readAllBytes(Paths.get("./www" + path)));
+        } catch (NoSuchFileException e) {
+            LOGGER.info("NO SE ENCONTRÓ EL ARCHIVO SOLICITADO. (" + path + ")");
+            errorCode = 404;
+        } catch (Exception e) {
+            LOGGER.info("ERROR INESPERADO!: " + e.getClass().getSimpleName());
+        }
+
+
+        //Esta línea sustituye el TARGET por el segundo string
+        // === COMENTADA PORQUE AHORITA NO SE PUEDE HACER REPLACE SOBRE UN ARRAY DE BYTES. TIENE QUE SER STRING.
+        
+
+        FileNameMap fileNameMap = URLConnection.getFileNameMap();
+        String fileContentType = fileNameMap.getContentTypeFor("file://" + path);
+        if (fileContentType == null) {
+            fileContentType = "application/octet-stream";
+        }
+        LOGGER.info(fileContentType);
+
+
+        /// SI ES TEXTO, SUSTITUIR LOS TARGETS Y LUEGO CONVERTIR A BYTES. SI ES CUALQUIER OTRA COSA (IMAGEN, FONT, ICON...) OBTENER BYTES DESDE EL ARCHIVO.
+        if(fileContentType.split("/")[0].equals("text")){
+            fileData = fileData.replace("{fieldTest_DEMO}", "El servidor cambió esto y agregó un número aleatorio: "+ (new Random()).nextInt(1000)  );
+            fileDataBytes = fileData.getBytes();
+        }else{
+            try {
+                fileDataBytes = Files.readAllBytes(Paths.get("./www" + path));
+            } catch (NoSuchFileException e) {
+                LOGGER.info("NO SE ENCONTRÓ EL ARCHIVO SOLICITADO. (" + path + ")");
+                errorCode = 404;
+            } catch (Exception e) {
+                LOGGER.info("ERROR INESPERADO!: " + e.getClass().getSimpleName());
+            }
+        }
+
+        /*LOGGER.info(path);
+        int i = path.lastIndexOf('.');
+        String extension = "";
+        if (i > 0) {
+            extension = path.substring(i+1);
+        }
+        LOGGER.info(extension);
+
+        if(extension.equals("ttf")){
+            fileContentType = "font/ttf";
+        }*/
+
+
+        ArrayList<String> responseHeaders = new ArrayList<>();
+        responseHeaders.add("HTTP/1.1 " + errorCode + " " + (errorCode == 200 ? "OK" : "Error"));
+        responseHeaders.add("Content-Type: " + fileContentType);
+        responseHeaders.add("ClaseCC8: Alumnos");
+        responseHeaders.add("Content-Length: " + fileDataBytes.length);
+        responseHeaders.add("");
+
+        // dataOut.print(responseHeaders.stream().collect(Collectors.joining("\r\n")) + "\r\n\r\n");
+        dataOut.print(responseHeaders.stream().collect(Collectors.joining("\r\n")) + "\r\n");
+        dataOut.write(fileDataBytes);
+        dataOut.flush();
+
+        if (!fileContentType.equals("application/octet-stream")) {
+            LOGGER.info("(" + nThreadServer + ") response: " + responseHeaders);
+            LOGGER.info("(" + nThreadServer + ") response: " + fileData);
+        }
+    }
 }
