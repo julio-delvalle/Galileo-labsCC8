6bda603cdb8f0663a9bf05b2d02e9429e131aea2
julio-delvalle <delvallejulio199@gmail.com>
2024-07-18 22:17:14 -0600

    POST methods finished, everything working correctly

--- Lab02_HTTP/Lab02/Response.java
+++ Lab02_HTTP/Lab02/Response.java
@@ -16,12 +16,23 @@ public class Response {
         LOGGER.info(request.toString());
         String[] requestArr = request.toString().split("\\|\\|", 0); 
 
+        String requestContentType = "";
+
+        for (int i = 0; i < requestArr.length; i++) {
+            if(requestArr[i].contains("Content-Type")){
+                requestContentType = requestArr[i].split(": ",2)[1];
+            }
+        }
+        LOGGER.info("requestContentType: "+requestContentType);
+
+
+
         LOGGER.info(requestArr[0]);
         LOGGER.info(requestArr[1]);
         LOGGER.info(requestArr[requestArr.length - 1]);
 
         String firstLine = requestArr[0];
-        String POSTbody = requestArr[requestArr.length - 1];
+        String POSTbody = requestArr[requestArr.length - 1].trim();
 
         String method = firstLine.split(" ")[0];
         String path = firstLine.split(" ")[1];
@@ -52,12 +63,76 @@ public class Response {
         String[] POSTparams = {};
         Map<String, String> POSTparamsPairs = new LinkedHashMap<String, String>();
         if (POSTbody.length() > 0 && method.equals("POST")) {
-            POSTparams = POSTbody.split("&");
-            for (String param : POSTparams) {
-                int idx = param.indexOf("=");
-                //Decodifica caracteres especiales de una vez.
-                POSTparamsPairs.put(URLDecoder.decode(param.substring(0, idx), "UTF-8"), URLDecoder.decode(param.substring(idx + 1), "UTF-8"));
+
+            if(requestContentType.contains("application/x-www-form-urlencoded")){
+                POSTparams = POSTbody.split("&");
+
+                LOGGER.info("DENTRO DE APPLICATION");
+                
+                for (String param : POSTparams) {
+                    int idx = param.indexOf("=");
+                    //Decodifica caracteres especiales de una vez.
+                    /*String paramName = URLDecoder.decode(param.substring(0, idx), "UTF-8");
+                    if(paramName.contains("name")){
+                        paramName = "name";
+                    }
+                    if(paramName.contains("email")){
+                        paramName = "email";
+                    }*/
+                    POSTparamsPairs.put(URLDecoder.decode(param.substring(0, idx), "UTF-8"), URLDecoder.decode(param.substring(idx + 1), "UTF-8"));
+                }
             }
+
+            if(requestContentType.contains("multipart/form-data")){
+                int boundaryidx = requestContentType.indexOf("boundary=");
+                String reqBoundary = requestContentType.substring(boundaryidx+9);//+9 para que no incluya la palabra boundary=
+                LOGGER.info("\n\n\n reqBoundary:"+reqBoundary+"\n\n\n");
+                POSTparams = POSTbody.split("\n");
+                
+                for (int i=1;i<POSTparams.length;i++) {
+                    int idx = 0;
+                    if(POSTparams[i].contains("name=\"")){
+                        idx = POSTparams[i].indexOf("name=\"");
+                        POSTparamsPairs.put(URLDecoder.decode(POSTparams[i].substring(idx+6, POSTparams[i].length()-2).trim(), "UTF-8"), URLDecoder.decode(POSTparams[i+2].trim(), "UTF-8"));
+                    }
+                }
+            }
+
+            if(requestContentType.contains("text/plain")){
+                POSTparams = POSTbody.split("\n");
+                
+                for (String param : POSTparams) {
+                    int idx = param.indexOf("=");
+                    POSTparamsPairs.put(URLDecoder.decode(param.substring(0, idx), "UTF-8"), URLDecoder.decode(param.substring(idx + 1), "UTF-8"));
+                }
+            }
+
+            if(requestContentType.contains("application/json")){
+                POSTbody = POSTbody.replace("{", "");
+                POSTbody = POSTbody.replace("}", "");
+
+                POSTparams = POSTbody.split(",");
+                
+                for (String param : POSTparams) {
+                    int idx = param.indexOf(":");
+                    POSTparamsPairs.put(URLDecoder.decode(param.substring(1, idx-1), "UTF-8"), URLDecoder.decode(param.substring(idx + 2, param.length()-1), "UTF-8"));
+                }
+            }
+
+            //// CONSIDERAR AQUÍ TODOS LOS CASOS DE POST
+            //// CONSIDERAR AQUÍ TODOS LOS CASOS DE POST
+            //// CONSIDERAR AQUÍ TODOS LOS CASOS DE POST
+            //// CONSIDERAR AQUÍ TODOS LOS CASOS DE POST
+            //// CONSIDERAR AQUÍ TODOS LOS CASOS DE POST
+            //// CONSIDERAR AQUÍ TODOS LOS CASOS DE POST
+            //// CONSIDERAR AQUÍ TODOS LOS CASOS DE POST
+            //// CONSIDERAR AQUÍ TODOS LOS CASOS DE POST
+
+
+
+            LOGGER.info("Post Params ARRAY: "+Arrays.toString(POSTparams));
+            LOGGER.info("Post Params ARRAY: "+POSTparams.length);
+
         }
         LOGGER.info("POST PAIRS MAP: "+POSTparamsPairs);
 
@@ -68,6 +143,9 @@ public class Response {
         
         if (path.endsWith("/")) {
             path += "index.html";
+        }
+        if (!path.contains(".")){
+            path += "/index.html";
         }
         
         
@@ -109,6 +187,7 @@ public class Response {
         LOGGER.info("GET MAP: "+GETparamsPairs.toString());
         LOGGER.info("POST PARAMS: "+POSTbody);
         LOGGER.info("POST PARAMS MAP: "+POSTparamsPairs.toString());
+        LOGGER.info("POST PARAMS MAP`KEYS: "+POSTparamsPairs.keySet().toString());
         /// SI ES TEXTO, SUSTITUIR LOS TARGETS Y LUEGO CONVERTIR A BYTES. SI ES CUALQUIER OTRA COSA (IMAGEN, FONT, ICON...) OBTENER BYTES DESDE EL ARCHIVO.
         if(fileContentType.split("/")[0].equals("text")){
             fileData = fileData.replace("{fieldTest_DEMO}", "El servidor cambió esto y agregó un número aleatorio: "+ (new Random()).nextInt(1000)  );
