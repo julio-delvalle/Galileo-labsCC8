81a74ae5cd28e936052b87ccd776638af4100c5d
julio-delvalle <delvallejulio199@gmail.com>
2024-07-31 14:47:07 -0600

    Lab04 initial commit

---
+++ Lab04_IMAP/Lab04/IMAPServer.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class IMAPServer {
+
+	private static final int PORT = 134; // Puerto IMAP
+	private static Map<String, String> users = new HashMap<>(); // Usuarios y contraseñas
+
+	public static void main(String[] args) {
+		// Agregar un usuario para la autenticación
+		users.put("jorge@lab03.com", "admin"); // Esto debe ser del SQLite
+
+		try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+			System.out.println("Servidor IMAP iniciado en el puerto " + PORT);
+
+			while (true) {
+				new ClientHandler(serverSocket.accept()).start();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static class ClientHandler extends Thread {
+		private Socket socket;
+		private PrintWriter out;
+		private BufferedReader in;
+
+		public ClientHandler(Socket socket) {
+			this.socket = socket;
+		}
+
+		@Override
+		public void run() {
+			try {
+				out = new PrintWriter(socket.getOutputStream(), true);
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+				// Enviar mensaje de bienvenida
+				out.println("* OK [CAPABILITY IMAP4rev1] IMAP Server Ready");
+
+				String inputLine;
+				boolean authenticated = false;
+
+				while ((inputLine = in.readLine()) != null) {
+					System.out.println("> " + inputLine );
+					if ( inputLine.matches("(?i)\\d+ login \"[^\"]+\" \"[^\"]+\"") ) {
+						String[] parts = inputLine.split(" ");
+						System.out.println("> " + parts[0] + " OK LOGIN completed" );
+					} else {
+						String[] parts = inputLine.split(" ");
+						System.out.println(parts[0] + " NO "+parts[1]+" failed" );
+						out.println(parts[0] + " NO "+parts[1]+" failed");
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
